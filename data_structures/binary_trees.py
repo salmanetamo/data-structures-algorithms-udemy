f903da9496d2d8e90aa617f97e27ba3629060ad8
Salmane <stamo15@alustudent.com>
2019-12-10 20:59:18 +0400

    Added Breadth First Search and Depth First Search algorithms in Binary Search tree implementation

--- data_structures/binary_trees.py
+++ data_structures/binary_trees.py
@@ -4,6 +4,9 @@ class Node:
         self.left = None
         self.right = None
 
+    def __str__(self):
+        return str(self.data)
+
     def __lt__(self, other):
         return self.data < other.data
 
@@ -57,6 +60,69 @@ class BinarySearchTree:
                 else:
                     current_node = current_node.right
 
+    def breadth_first_search(self):
+        current_node = self.root
+        result = []
+        queue = list()
+
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            result.append(current_node)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+
+        return result
+
+    def breadth_first_search_recursive(self, queue, result):
+        if len(queue) == 0:
+            return result
+
+        current_node = queue.pop(0)
+        result.append(current_node)
+        if current_node.left is not None:
+            queue.append(current_node.left)
+        if current_node.right is not None:
+            queue.append(current_node.right)
+
+        return self.breadth_first_search_recursive(queue, result)
+
+    def dfs_in_order(self):
+        def in_order(node, result):
+            if node.left is not None:
+                in_order(node.left, result)
+            result.append(node)
+            if node.right is not None:
+                in_order(node.right, result)
+            return result
+
+        return in_order(self.root, [])
+
+    def dfs_pre_order(self):
+        def pre_order(node, result):
+            result.append(node)
+            if node.left is not None:
+                pre_order(node.left, result)
+            if node.right is not None:
+                pre_order(node.right, result)
+            return result
+
+        return pre_order(self.root, [])
+
+    def dfs_post_order(self):
+        def post_order(node, result):
+            if node.left is not None:
+                post_order(node.left, result)
+            if node.right is not None:
+                post_order(node.right, result)
+            result.append(node)
+            return result
+
+        return post_order(self.root, [])
+
+
 
 
 def traverse(node):
@@ -72,6 +138,7 @@ def traverse(node):
     if node.right is not None:
         traverse(node.right)
 
+
 bst = BinarySearchTree()
 bst.insert(9)
 bst.insert(4)
@@ -80,5 +147,18 @@ bst.insert(20)
 bst.insert(170)
 bst.insert(15)
 bst.insert(1)
-print(bst.lookup(170))
+# print(bst.lookup(170))
 # traverse(bst.root)
+# for node in bst.breadth_first_search():
+#     print(node)
+# for node in bst.breadth_first_search_recursive([bst.root], []):
+#     print(node)
+
+# for node in bst.dfs_in_order():
+#     print(node)
+
+# for node in bst.dfs_pre_order():
+#     print(node)
+
+for node in bst.dfs_post_order():
+    print(node)
